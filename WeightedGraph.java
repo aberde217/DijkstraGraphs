6951da3e9681956d026c2770b60d808e9769d0f7
aberde217 <115136078+aberde217@users.noreply.github.com>
2023-11-17 10:20:29 -0800

    Update WeightedGraph.java

--- WeightedGraph.java
+++ WeightedGraph.java
@@ -20,21 +20,21 @@ public class WeightedGraph {
     private void createEdge(int v, int w, int dist) {
         for (Edge e: adjList[v]) {
             if (e.w == w && e.dist == dist) return;
-            else if (e.w == w && e.dist != dist) {
-                e.dist = dist;
-                return;
-            }
         }
         adjList[v].add(new Edge(w, dist));
     }
-
-    public int distanceToVertex(int v) {
+    public int DijkstraSearch(int v) {
         DijkstraPath path = new DijkstraPath(0);
         return path.distTo[v];
     }
 
+    public int AStarSearch(int v) {
+        AStar path = new AStar(0, v);
+        return path.distTo[v];
+    }
+
     public List<Integer> pathTo(int v) {
-        DijkstraPath path = new DijkstraPath(0);
+        AStar path = new AStar(0, v);
         List<Integer> pathList = new ArrayList<>();
         int L = v;
         while (L != 0) {
@@ -45,7 +45,7 @@ public class WeightedGraph {
         return pathList;
     }
 
-    private class Pair implements Comparable<Pair> {
+    protected class Pair implements Comparable<Pair> {
         private int v, disTo;
         public Pair(int v, int distTo) {
             this.v = v;
@@ -80,13 +80,13 @@ public class WeightedGraph {
             search(s);
         }
         private void search(int s) {
-            MinPQ minPQ = new MinPQ();
+            PairMinPQ minPQ = new PairMinPQ();
             for (int i = 0; i < V; i++) {
                 Pair p = new Pair(i, distTo[i]);
                 minPQ.add(p);
             }
             Pair smallest = minPQ.getSmallest();
-            while(minPQ.size != 0) {
+            while(minPQ.size() != 0) {
                 minPQ.removeSmallest();
                 for (Edge e: adjList[smallest.v]) {
                     Pair pair = relaxEdge(e, smallest.v);
@@ -107,86 +107,62 @@ public class WeightedGraph {
             return p;
         }
     }
-    private class MinPQ {
-        private Pair[] arr;
-        private int size = 0;
-        private static final int INITIAL_SIZE = 10;
 
-        public MinPQ() {
-            arr = new Pair[INITIAL_SIZE];
-            arr[0] = null;
+    public class AStar {
+        private int[] distTo;
+        private int[] edgeTo;
+        private int[] distToTarget;
+        public int s, t;
+
+        public AStar(int source, int target) {
+            s = source;
+            t = target;
+            edgeTo = new int[V];
+            distTo = new int[V];
+            distToTarget = new int[V];
+            for (int i = 0; i < V; i++) {
+                if (i == 0) distTo[i] = 0;
+                else distTo[i] = Integer.MAX_VALUE;
+
+                if (i == t) distToTarget[i] = 0;
+                else distToTarget[i] = smallestWeight(i);
+            }
+
+            search(source);
+        }
+        private int smallestWeight(int v) {
+            int smallestDist = Integer.MAX_VALUE;
+            for (Edge e: adjList[v]) {
+                if (e.dist < smallestDist && e.w > v) smallestDist = e.dist;
+            }
+            return smallestDist;
         }
 
-        private void swap(int child, int parent) {
-            Pair copy = arr[parent];
-            arr[parent] = arr[child];
-            arr[child] = copy;
-        }
-        private int parentIndex(int index) {
-            if (index/2 == 0) return 1;
-            return index/2;
-        }
-
-        private int rightChildIndex(int index) {
-            return (index * 2) + 1;
-        }
-
-        private int leftChildIndex(int index) {
-            return (index * 2);
-        }
-        private void swimUp(int index) {
-            if (index == 1);
-            else if (arr[index].compareTo(arr[parentIndex(index)]) <= 0) {
-                swap(index, parentIndex(index));
-                swimUp(parentIndex(index));
+        private void search(int s) {
+            PairMinPQ minPQ = new PairMinPQ();
+            for (int i = 0; i < V; i++) {
+                minPQ.add(new Pair(i, distTo[i]));
+            }
+            int smallest = minPQ.getSmallest().v;
+            while(minPQ.getSmallest().v != t) {
+                minPQ.removeSmallest();
+                for (Edge e : adjList[smallest]) {
+                    Pair p = relaxEdge(e, smallest);
+                    if (p != null)  minPQ.add(p);
+                }
+                smallest = minPQ.getSmallest().v;
             }
         }
 
-        private void swimDown(int index) {
-            if (arr[leftChildIndex(index)] == null && arr[rightChildIndex(index)] == null);
-            else if (arr[leftChildIndex(index)] != null && arr[index].compareTo(arr[leftChildIndex(index)]) >= 0) {
-                swap(leftChildIndex(index),index);
-                swimDown(leftChildIndex(index));
-            } else if (arr[rightChildIndex(index)] != null && arr[index].compareTo(arr[rightChildIndex(index)]) >= 0) {
-                swap(rightChildIndex(index),index);
-                swimDown(rightChildIndex(index));
+        private Pair relaxEdge(Edge e, int v) {
+            Pair p = null;
+            if (distTo[v] + e.dist < distTo[e.w]) {
+                distTo[e.w] = e.dist + distTo[v];
+                edgeTo[e.w] = v;
+                p = new Pair(e.w, distTo[e.w] + distToTarget[e.w]);
             }
-        }
-
-        private void resizeUp() {
-            Pair[] newArr = new Pair[arr.length * 2];
-            System.arraycopy(arr, 0, newArr, 0, size + 1);
-            arr = newArr;
-        }
-        private void resizeDown() {
-            Pair[] newArr = new Pair[arr.length/2];
-            System.arraycopy(arr, 0, newArr, 0, size + 1);
-            arr = newArr;
-        }
-        public void add(Pair x) {
-            if ((double)size/arr.length >= ((double)4/5)) {
-                resizeUp();
-            }
-            arr[size + 1] = x;
-            size += 1;
-            int index = size;
-            swimUp(index);
-        }
-
-        public Pair getSmallest() {
-            return arr[1];
-        }
-
-        public Pair removeSmallest() {
-            Pair smallest = getSmallest();
-            swap(size, 1);
-            arr[size] = null;
-            swimDown(1);
-            size--;
-            if ((double)size/arr.length <= ((double)1/6)) resizeDown();
-            return smallest;
+            return p;
         }
 
     }
-
 }
